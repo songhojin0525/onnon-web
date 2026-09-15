dc5915126d7f41cb492f56d55f37b9387506cb02
songhojin0525 <86887609+songhojin0525@users.noreply.github.com>
2026-09-15 11:08:28 +0900

    Initialize Supabase client in supabase.js

---
+++ public/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
+import config from './config.js'
+
+const supabase = createClient(
+  config.SUPABASE_URL,
+  config.SUPABASE_ANON_KEY
+)
+
+export default supabase
